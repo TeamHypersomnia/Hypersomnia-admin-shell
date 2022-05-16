aefa6790b8c66e8cc906c250ad447e054d81c8cf
geneotech <patryk.czachurski@gmail.com>
2022-05-16 20:56:42 +0200

    Renames for clarity

--- set_latest_version.sh
+++ set_latest_version.sh
@@ -1,7 +1,7 @@
 . ./ADDRESS
-DEPLOY_KEY=$1
+API_KEY=$1
 VERSION=$2
 SET_LATEST_URL="$DOWNLOADS_HOST/set_latest_version.php"
 
-curl -F "key=$DEPLOY_KEY" -F "version=$VERSION" $SET_LATEST_URL
+curl -F "key=$API_KEY" -F "version=$VERSION" $SET_LATEST_URL
 ./sync_and_update.sh
