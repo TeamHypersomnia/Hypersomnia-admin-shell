5b7bb52f11fb18de57ccc18626ec593b174688e2
geneotech <patryk.czachurski@gmail.com>
2022-05-21 21:01:53 +0200

    Save the last signing key used to sign the files

--- developer_sign_file.sh
+++ developer_sign_file.sh
@@ -2,5 +2,6 @@ developer_sign_file() {
 	local SIGN_NAMESPACE=$1
 	local FILE_PATH=$2
 
-	trezor-agent -e ed25519 hypersomnia -- ssh-keygen -vvv -Y sign -f ~/.ssh/trezor-hypersomnia.pub -n $SIGN_NAMESPACE $FILE_PATH
+	trezor-agent -e ed25519 hypersomnia > used_signing_key.pub
+	trezor-agent -e ed25519 hypersomnia -- ssh-keygen -vvv -Y sign -f used_signing_key.pub -n $SIGN_NAMESPACE $FILE_PATH
 }
