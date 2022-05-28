8c2b119b463a819ccb57dee3540c841ad161fffe
geneotech <patryk.czachurski@gmail.com>
2022-05-29 00:02:13 +0200

    Properly sign builds

--- developer_sign_file.sh
+++ developer_sign_file.sh
@@ -2,6 +2,15 @@ developer_sign_file() {
 	local SIGN_NAMESPACE=$1
 	local FILE_PATH=$2
 
-	trezor-agent -e ed25519 hypersomnia > used_signing_key.pub
-	trezor-agent -e ed25519 hypersomnia -- ssh-keygen -vvv -Y sign -f used_signing_key.pub -n $SIGN_NAMESPACE $FILE_PATH
+	local PUBKEY_PATH="used_signing_key.pub"
+
+	if [ ! -f "$PUBKEY_PATH" ]; then
+		local PUBLIC_KEY=$(trezor-agent -e ed25519 hypersomnia)
+		echo "Public key used to sign binaries:"
+		echo $PUBLIC_KEY
+
+		echo $PUBLIC_KEY > $PUBKEY_PATH
+	fi
+
+	trezor-agent -e ed25519 hypersomnia -- ssh-keygen -vvv -Y sign -f $PUBKEY_PATH -n $SIGN_NAMESPACE $FILE_PATH
 }
