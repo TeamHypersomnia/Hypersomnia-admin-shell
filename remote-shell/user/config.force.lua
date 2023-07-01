db7220943a0e66607e503095425e8e339d0949bd
geneotech <patryk.czachurski@gmail.com>
2023-07-01 19:59:07 +0200

    Update config.force.lua to the new server config spec

--- remote-shell/user/config.force.lua
+++ remote-shell/user/config.force.lua
@@ -4,25 +4,28 @@ return {
   http_client = {
     update_on_launch = false
   },
+
   unit_tests = {
     run = false
   },
-  private_server = {
-    master_rcon_password = read_password(".rcon_password"),
-    rcon_password = read_password(".rcon_password"),
-    discord_webhook_url = read_password(".discord_webhook_url"),
-    telegram_webhook_url = read_password(".telegram_webhook_url")
-  },
+
   server_start = {
     port = 8412
   },
+
+  server_private = {
+    rcon_password = read_password(".rcon_password"),
+    master_rcon_password = read_password(".master_rcon_password"),
+    discord_webhook_url = read_password(".discord_webhook_url"),
+    telegram_webhook_url = read_password(".telegram_webhook_url")
+  },
+
   server = {
+    arena = "de_cyberaqua",
+    mode = "gun_game",
     server_name = "arena.hypersomnia.xyz",
     max_client_resyncs = 1000,
     allow_nat_traversal = false,
     suppress_new_community_server_webhook = true
-  },
-  server_solvable = {
-    current_arena = "de_cyberaqua"
   }
 }
