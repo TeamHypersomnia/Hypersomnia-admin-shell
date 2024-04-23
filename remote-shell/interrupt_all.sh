5149721dbf9e8687e78e1ecc0b14d2055b99f406
geneotech <patryk.czachurski@gmail.com>
2024-04-23 16:54:32 +0200

    Host non-ranked PL server for the Web

--- remote-shell/interrupt_all.sh
+++ remote-shell/interrupt_all.sh
@@ -1,4 +1,2 @@
-echo "Interrupting the dedicated server."
-pkill -f --signal=SIGINT dedicated
-echo "Interrupting the masterserver."
-pkill -f --signal=SIGINT masterserver
+echo "Interrupting all servers."
+pkill -f --signal=SIGINT Headless
--- remote-shell/run_dedicated.sh
+++ remote-shell/run_dedicated.sh
@@ -1,3 +1,4 @@
 echo "Launching the dedicated server."
 ulimit -c unlimited
-nohup ~/Hypersomnia-Headless.AppImage --dedicated-server > /dev/null 2>&1 &
+nohup ~/Hypersomnia-Headless.AppImage > /dev/null 2>&1 &
+nohup ~/Hypersomnia-Headless.AppImage --apply-config ~/servers/config.common.lua --appdata-dir ~/servers/1 > /dev/null 2>&1 &
